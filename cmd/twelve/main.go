c366a3ac1858a3c44174f0527c22df8474c1049d
Krivonos Ivan <devbackend@yandex.ru>
2020-02-12 20:33:08 +0300

    [Factor #3] Env variables

--- cmd/twelve/main.go
+++ cmd/twelve/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"go.uber.org/zap"
+	"os"
+)
 
+func main() {
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		logger.Fatal("Empty port")
+	}
+
+	dsn := os.Getenv("DSN")
+	if dsn == "" {
+		logger.Fatal("Empty dsn")
+	}
 }
